6f744da0512431e83d49776c814b1656bb23a293
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-25 20:03:33 -0600

    Created basic data structure for Plots

---
+++ Graphs/PlotInfo/LinePlot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.PlotInfo
+{
+    public class LinePlot
+    {
+        private List<Point> Points;
+        public static event EventHandler PlotUpdate;
+
+        public LinePlot()
+        {
+            Points = new List<Point>();
+        }
+
+        public void AddPoint(Point point)
+        {
+            Points.Add(point);
+            OnPlotUpdate(this, EventArgs.Empty);
+        }
+
+        public void SetPoints(List<Point> points)
+        {
+            Points = points;
+            OnPlotUpdate(this, EventArgs.Empty);
+        }
+
+        private void OnPlotUpdate(object sender, EventArgs e)
+        {
+            PlotUpdate?.Invoke(sender, e);
+        }
+    }
+}
---
+++ Graphs/PlotInfo/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.PlotInfo
+{
+    public class Point
+    {
+        public double X_Value { get; private set; } 
+        public double Y_Value { get; private set; }
+
+        public Point(double x, double y) 
+        {
+            X_Value = x;
+            Y_Value = y;
+        }
+
+        public void SetValues(double x, double y)
+        {
+            X_Value = x;
+            Y_Value = y;
+        }
+    }
+}
